95d23e8c3f4c2de48f7f926c926fb3654bf0b8a3
Peter Horvath <peter.horvath@emarsys.com>
2020-12-01 06:42:27 +0100

    minor refactor

--- 1.py
+++ 1.py
@@ -9,10 +9,9 @@ def expense(index):
 
 if __name__ == '__main__':
     lines = read_lines("1.txt")
-    result = 0
-    for i in range(0,len(lines)):
-        for j in range(i,len(lines)):
+    for i in range(0, len(lines)):
+        for j in range(i, len(lines)):
             for k in range(j, len(lines)):
                 if expense(i) + expense(j) + expense(k) == 2020:
-                    result = expense(i) * expense(j) * expense(k)
-    print(result)
+                    print(expense(i) * expense(j) * expense(k))
+                    break
