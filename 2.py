c1d93f40ab158845d491fac82b6bec654e4862bd
Peter Horvath <peter.horvath@emarsys.com>
2020-12-02 20:04:29 +0100

    22

---
+++ 2.py
@@ -0,0 +1,22 @@
+def read_lines(file_name):
+    with open(file_name) as fp:
+        return fp.readlines()
+
+
+def expense(index):
+    return int(lines[index])
+
+
+if __name__ == '__main__':
+    lines = read_lines("2.txt")
+    total = 0
+    for line in lines:
+        policy = line.split(':')[0]
+        num = policy.split(" ")[0]
+        p1 = int(num.split('-')[0])
+        p2 = int(num.split('-')[1])
+        letter = policy.split(" ")[1]
+        pwd = line.split(':')[1]
+        if (pwd[p1] == letter and pwd[p2] != letter) or (pwd[p1] != letter and pwd[p2] == letter):
+            total += 1
+    print(total)
