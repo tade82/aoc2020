e738329ff497a507d90ff8cbc6989270c5471c0c
Peter Horvath <peter.horvath@emarsys.com>
2020-12-03 09:28:28 +0100

    use Coordinates instead of Pos

--- 3.py
+++ 3.py
@@ -1,6 +1,7 @@
-import collections
-
 import numpy
+from coordinates import Coordinate
+
+Coordinate.default_order = 'xy'
 
 
 def read_lines(file_name):
@@ -8,29 +9,22 @@ def read_lines(file_name):
         return fp.read().splitlines()
 
 
-Pos = collections.namedtuple('Pos', 'x y')
-
-
-def move(a: Pos, b: Pos):
-    return Pos(a.x + b.x, a.y + b.y)
-
-
 if __name__ == '__main__':
     lines = read_lines("3.txt")
     mod = len(lines[0])
-    slopes = [Pos(1, 1), Pos(3, 1), Pos(5, 1), Pos(7, 1), Pos(1, 2)]
-    pos = Pos(0, 0)
-    tree_per_slope = {}
+    slopes = [Coordinate(1, 1), Coordinate(3, 1), Coordinate(5, 1), Coordinate(7, 1), Coordinate(1, 2)]
+    pos = Coordinate(0, 0)
+    trees = []
     for slope in slopes:
         count = 0
         while pos.y < len(lines):
             if lines[pos.y][pos.x % mod] == '#':
                 count += 1
-            pos = move(pos, slope)
+            pos = pos + slope
 
         print(f'{slope} -> {count}')
-        pos = Pos(0, 0)
-        tree_per_slope[slope] = count
+        pos = Coordinate(0, 0)
+        trees.append(count)
 
-    result = numpy.prod(list(tree_per_slope.values()))
+    result = numpy.prod(trees)
     print(f'result: {result}')
