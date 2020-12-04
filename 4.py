95b1170d493e2b143239fd101105b4f471dd10b4
Peter Horvath <peter.horvath@emarsys.com>
2020-12-04 10:19:17 +0100

    minor refactor

--- 4.py
+++ 4.py
@@ -12,21 +12,21 @@ def validate(passports, with_value_check=False):
         'iyr': (lambda x: 2010 <= int(x) <= 2020),
         'eyr': (lambda x: 2020 <= int(x) <= 2030),
         'hgt': (lambda x: 150 <= int(x[:-2]) <= 193 if x[-2:] == 'cm' else 59 <= int(x[:-2]) <= 76 if x[-2:] == 'in' else False),
-        'hcl': (lambda x: re.search('^#[0-9a-f]{6}$', x) is not None),
+        'hcl': (lambda x: bool(re.search('^#[0-9a-f]{6}$', x))),
         'ecl': (lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']),
-        'pid': (lambda x: re.search('^[0-9]{9}$', x) is not None),
+        'pid': (lambda x: bool(re.search('^[0-9]{9}$', x))),
         'cid': (lambda x: True)
     }
     result = 0
     for passport in passports:
-        fields = {'cid': None}
+        fields = {}
         for field in passport.replace('\n', ' ').split(' '):
             fk = field.split(':')[0]
             fv = field.split(':')[1]
             if not with_value_check or rules[fk](fv):
                 fields[fk] = fv
 
-        if set(rules.keys()).issubset(fields.keys()):
+        if set(rules).symmetric_difference(set(['cid'])).issubset(fields):
             result += 1
     return result
 
