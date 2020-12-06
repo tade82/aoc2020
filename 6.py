74955a49b71c62efea1acb653de26263a8faec03
Peter Horvath <peter.horvath@emarsys.com>
2020-12-06 07:39:43 +0100

    refactor

--- 6.py
+++ 6.py
@@ -1,24 +1,24 @@
 import string
 
 
-def read_lines(file_name):
+def read_groups(file_name):
     with open(file_name) as fp:
-        return fp.read()
+        for lines in fp.read().split("\n\n"):
+            yield lines.splitlines()
 
 
 if __name__ == '__main__':
-    lines = read_lines("6.txt")
-    asked_total = 0
-    commonly_asked_total = 0
-    groups = map(lambda x: x.splitlines(), lines.split("\n\n"))
+    groups = read_groups("6.txt")
+    anyone_asked_count = 0
+    everyone_asked_count = 0
     for group in groups:
-        asked_questions = set()
-        commonly_asked_questions = set(list(string.ascii_lowercase))
+        anyone_asked = set()
+        everyone_asked = set(list(string.ascii_lowercase))
         for person in group:
-            asked_questions = asked_questions.union(set(list(person)))
-            commonly_asked_questions = commonly_asked_questions.intersection(set(list(person)))
-        asked_total += len(asked_questions)
-        commonly_asked_total += len(commonly_asked_questions)
+            anyone_asked = anyone_asked.union(set(list(person)))
+            everyone_asked = everyone_asked.intersection(set(list(person)))
+        anyone_asked_count += len(anyone_asked)
+        everyone_asked_count += len(everyone_asked)
 
-    print(f'1st result: {asked_total}')
-    print(f'2nd result: {commonly_asked_total}')
+    print(f'1st result: {anyone_asked_count}')
+    print(f'2nd result: {everyone_asked_count}')
