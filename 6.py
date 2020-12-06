f8fe5c82a6ead68c52e4477c5ad165c8946141d2
Peter Horvath <peter.horvath@emarsys.com>
2020-12-06 07:54:31 +0100

    refactor

--- 6.py
+++ 6.py
@@ -9,16 +9,16 @@ def read_groups(file_name):
 
 if __name__ == '__main__':
     groups = read_groups("6.txt")
-    anyone_asked_count = 0
-    everyone_asked_count = 0
+    anyone_count = 0
+    everyone_count = 0
     for group in groups:
-        anyone_asked = set()
-        everyone_asked = set(list(string.ascii_lowercase))
+        anyone = set()
+        everyone = set(list(string.ascii_lowercase))
         for person in group:
-            anyone_asked = anyone_asked.union(set(list(person)))
-            everyone_asked = everyone_asked.intersection(set(list(person)))
-        anyone_asked_count += len(anyone_asked)
-        everyone_asked_count += len(everyone_asked)
+            anyone = anyone.union(set(list(person)))
+            everyone = everyone.intersection(set(list(person)))
+        anyone_count += len(anyone)
+        everyone_count += len(everyone)
 
-    print(f'1st result: {anyone_asked_count}')
-    print(f'2nd result: {everyone_asked_count}')
+    print(f'1st result: {anyone_count}')
+    print(f'2nd result: {everyone_count}')
